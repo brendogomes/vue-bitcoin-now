80542f13adb6dc2b87e03bb1c601924266fa0a19
brendogomes <brendo.log@gmail.com>
2024-03-07 22:19:55 -0300

    [feat] project vue-bitcoin-now

---
+++ src/App.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+import BitcoinQuote from './components/BitcoinQuote.vue'
+import { ref, onMounted } from 'vue'
+
+let responseData = ref(null);
+
+onMounted(() => {
+  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then((response) => {
+    responseData.value = response.data;
+  }).catch((error) => {
+    console.error(error);
+  }) 
+}) 
+</script>
+
+<template>
+  <div>
+    <img src="./assets/Bitcoin.svg.png" class="logo" alt="Vite logo" />
+  </div>
+  <BitcoinQuote :responseView="responseData" v-if="responseData" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+
+.logo:hover {
+  filter: drop-shadow(0 0 1em #ffd45daa);
+}
+</style>
---
+++ src/components/BitcoinQuote.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+const props = defineProps<{ responseView }>()
+</script>
+
+<template>
+  <section id="section-container">
+    <div v-for="quote in props.responseView.bpi" class="card">
+      <p v-html="quote.symbol"></p>
+      <div>
+        <h2>{{ quote.rate }}</h2>
+      </div>
+    </div>
+  </section>
+
+  <small>{{ props.responseView.disclaimer }}</small>
+</template>
+
+<style scoped>
+#section-container {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+#section-container .card {
+  background-color: #383838;
+  border-radius: 12px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: 26px;
+}
+
+.card:nth-child(2) {
+  margin: 0 10px;
+  margin-bottom: 26px;
+}
+
+h2 {
+  margin: 0 0 0 10px;
+}
+
+p {
+  font-size: 2em;
+  font-weight: 500;
+  text-align: left;
+  margin: 0;
+}
+</style>
